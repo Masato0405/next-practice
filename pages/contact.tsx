8be0cb32c018b2692842353fae1e72a4cfe5bcae
MasatoIso <04masato05@gmail.com>
2021-11-02 08:20:59 +0900

    add React Hook Form pakcage

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -3,30 +3,51 @@ import { useState } from "react";
 
 const Contact: NextPage = () => {
   const [name, setName] = useState("あいうえお");
-  const [mail, setMail] = useState("");
-  const [content, setContent] = useState("");
+  const [mail, setMail] = useState("test@test.jp");
+  const [content, setContent] = useState("入力してください");
+
+  const submitForm = () => {
+    alert(`お名前:${name}`);
+    alert(`メールアドレス:${mail}`);
+    alert(`問い合わせ内容:${content}`);
+  };
 
   return (
     <form>
       <div>
         <label htmlFor="">
           お名前:
-          <input type="text" value={name} />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
       </div>
       <div>
         <label htmlFor="">
           メールアドレス:
-          <input type="text" />
+          <input
+            type="text"
+            value={mail}
+            onChange={(e) => setMail(e.target.value)}
+          />
         </label>
       </div>
       <div>
         <label htmlFor="">
           問い合わせ内容:
-          <textarea name="" id="" cols={30} rows={10}></textarea>
+          <textarea
+            cols={30}
+            rows={10}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></textarea>
         </label>
       </div>
-      <button type="button">送信</button>
+      <button type="button" onClick={() => submitForm()}>
+        送信
+      </button>
     </form>
   );
 };
---
+++ pages/form.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from "next";
+
+// Test page to test React Hook Form
+const form: NextPage = () => <h1>テスト</h1>;
+
+export default form;
