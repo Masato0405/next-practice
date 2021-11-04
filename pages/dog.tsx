fa3dfc05046ca006aa7d66b84c6e535203fefc5a
MasatoIso <04masato05@gmail.com>
2021-11-05 08:13:59 +0900

    fetch breed list

--- pages/dog.tsx
+++ pages/dog.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
-const Dog: NextPage = () => {
-  return <h1>Dog Page</h1>;
+export async function getStaticProps() {
+  const res = await fetch("https://dog.ceo/api/breeds/list/all");
+  const data = await res.json();
+
+  console.log(data.message);
+  return {
+    props: { data },
+  };
+}
+
+const Dog: NextPage = (props) => {
+  return <h1>hoge</h1>;
 };
 
 export default Dog;
