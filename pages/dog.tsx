9f9fe1e0e6edec2b53e4fc1bcf8b989a1106a5fd
MasatoIso <04masato05@gmail.com>
2021-11-06 08:05:00 +0900

    extract breed

--- pages/dog.tsx
+++ pages/dog.tsx
@@ -4,13 +4,15 @@ export async function getStaticProps() {
   const res = await fetch("https://dog.ceo/api/breeds/list/all");
   const data = await res.json();
 
-  console.log(data.message);
   return {
     props: { data },
   };
 }
 
-const Dog: NextPage = (props) => {
+const Dog: NextPage = ({ data }) => {
+  const breeds = Object.entries(data.message);
+  console.log(breeds);
+
   return <h1>hoge</h1>;
 };
 
